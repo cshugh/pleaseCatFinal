1cf32b9da0fb37d8ad2872395718344a3844a92a
sinoni <seouland2@hanmail.net>
2020-02-14 16:39:00 +0900

    2020-02-14 front/post 에러 해결

--- PleaseCat/front/src/components/map/selectlLocationMap.vue
+++ PleaseCat/front/src/components/map/selectlLocationMap.vue
@@ -1,17 +1,17 @@
 <template>
 <div id="selectMap">
     <p style="margin-top:-12px">
-        <b>Chrome 브라우저는 https 환경에서만 geolocation을 지원합니다.</b> 참고해주세요.
+        <!-- <b>Chrome 브라우저는 https 환경에서만 geolocation을 지원합니다.</b> 참고해주세요. -->
     </p>
-    <div id="map" style="width:100%;height:350px;"></div>
-    <div>
+    <div id="map" style="width:100%;height:240px;"></div>
+    <!-- <div>
         위도: <span id="lat">{{ mark_x }}</span>
     </div>
     <div>
         경도: <span id="lng">{{ mark_y }}</span>
-    </div>
+    </div> -->
     <div>
-        주소: <span id="address">{{ address }}</span>
+        <span id="address"><p></p>{{ address }}</span>
     </div>
 </div>
 </template>
