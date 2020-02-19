c7196c385a3ed308234c7154bcd4ee6870c59949
kingjky <kingjky@naver.com>
2020-02-19 19:36:27 +0900

    Add import material

--- PleaseCat/front/src/main.js
+++ PleaseCat/front/src/main.js
@@ -5,8 +5,15 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 import moment from 'moment'
 import infiniteScroll from "vue-infinite-scroll";
+import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
 window.Vue = Vue;
 Vue.use(infiniteScroll);
+Vue.use(MdButton)
+Vue.use(MdContent)
+Vue.use(MdTabs)
 Vue.config.productionTip = false
 
 Vue.prototype.moment = moment
